eb9949e5fbf86d37358df4b22e1f6001cf4c5ffe
Sammy Yang <syang142@uw.edu>
2024-03-10 18:45:51 -0700

    Add player death and reset scene mechanics

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,17 +6,12 @@ public class EnemyController : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Rigidbody player;
-
-    private float walkingInc = 0.5f;
-    private float walkingReset = 0;
-
-    private float animationDegree = 40f;
-    
-    private bool isHit = false;
-
     public float lookRotateSpeed = 1000f;
 
-
+    private float _walkingInc = 0.5f;
+    private float _walkingReset = 0;
+    private float _animationDegree = 40f;
+    private bool _isHit;
     
     void Start()
     {
@@ -31,7 +26,7 @@ public class EnemyController : MonoBehaviour
 
     void LateUpdate()
     {
-        if (isHit)
+        if (_isHit)
         {
             agent.enabled = false;
         }
@@ -39,24 +34,19 @@ public class EnemyController : MonoBehaviour
         {
             agent.enabled = true;
             agent.SetDestination(player.position);
-            
-            
             WalkingAnimation();
-            
         }
-
-        isHit = false;
+        _isHit = false;
     }
     
     public void GotHit()
     {
-        Debug.Log("I got hit");
-        isHit = true;
+        _isHit = true;
     }
 
     void RotateTowardsWalking()
     {
-         if (agent.velocity != Vector3.zero) // Ensure the agent is moving before rotating
+        if (agent.velocity != Vector3.zero)
         {
             Vector3 direction = (agent.steeringTarget - transform.position).normalized;
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
@@ -66,27 +56,26 @@ public class EnemyController : MonoBehaviour
 
     void WalkingAnimation() {
 
-        if (Math.Abs(walkingReset) > animationDegree) {
-            walkingInc = -walkingInc;
+        if (Math.Abs(_walkingReset) > _animationDegree) {
+            _walkingInc = -_walkingInc;
         }
-        transform.Find("BodyFrame").Find("Body").Rotate(-walkingInc * Vector3.up / 10);
+        transform.Find("BodyFrame").Find("Body").Rotate(-_walkingInc * Vector3.up / 10);
         // Limping leg
-        transform.Find("BodyFrame").Find("LimpingLegFrame").Rotate(-walkingInc * Vector3.forward);
+        transform.Find("BodyFrame").Find("LimpingLegFrame").Rotate(-_walkingInc * Vector3.forward);
         // Working leg
-        transform.Find("BodyFrame").Find("WalkingLegFrame").Rotate(walkingInc * Vector3.forward);
-        if (walkingReset > 0) {
-            transform.Find("BodyFrame").Find("WalkingLegFrame").Find("WalkingShinFrame").Rotate(-walkingInc * Vector3.forward);
+        transform.Find("BodyFrame").Find("WalkingLegFrame").Rotate(_walkingInc * Vector3.forward);
+        if (_walkingReset > 0) {
+            transform.Find("BodyFrame").Find("WalkingLegFrame").Find("WalkingShinFrame").Rotate(-_walkingInc * Vector3.forward);
         }
         // Arms
-        transform.Find("BodyFrame").Find("RightArmFrame").Rotate(-walkingInc * Vector3.forward / 10);
-        transform.Find("BodyFrame").Find("LeftArmFrame").Rotate(walkingInc * Vector3.forward / 10);
+        transform.Find("BodyFrame").Find("RightArmFrame").Rotate(-_walkingInc * Vector3.forward / 10);
+        transform.Find("BodyFrame").Find("LeftArmFrame").Rotate(_walkingInc * Vector3.forward / 10);
         // Head
-        transform.Find("BodyFrame").Find("HeadFrame").Rotate(-walkingInc * Vector3.forward / 10);
+        transform.Find("BodyFrame").Find("HeadFrame").Rotate(-_walkingInc * Vector3.forward / 10);
         // Reset
-        if (walkingReset == 0 && walkingInc > 0) {
-            transform.Find("BodyFrame").Find("WalkingLegFrame").Find("WalkingShinFrame").Rotate(-walkingInc * Vector3.forward);
+        if (_walkingReset == 0 && _walkingInc > 0) {
+            transform.Find("BodyFrame").Find("WalkingLegFrame").Find("WalkingShinFrame").Rotate(-_walkingInc * Vector3.forward);
         }
-
-        walkingReset += walkingInc;
+        _walkingReset += _walkingInc;
     }
 }
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,17 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
     public float speed = 2.0f;
     public Vector3 worldPosition;
-    Plane plane = new Plane(Vector3.up, 0); // plane that the player points to
+    public Camera mainCamera;
+    public GameObject gameOverScreen;
+    
+    private Plane _plane = new Plane(Vector3.up, 0); // plane that the player points to
+    private const float RotationSpeed = 5f;
+    private bool _isDead;
 
-    float rotationSpeed = 5f;
+    private void Start()
+    {
+        gameOverScreen.SetActive(false);
+    }
 
-     public Camera mainCamera;
-     
     void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
@@ -19,35 +27,35 @@ public class PlayerMover : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
 
-        // Apply the movement to the sphere's position
         transform.position += Time.deltaTime * movement * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Monster"))
+        {
+            Debug.Log("I should be dead");
+            gameOverScreen.SetActive(true);
+        }
     }
 
     void Update()
     {
-          float distance;
+        float distance;
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        // checks if ray hit the plane
-        // could be changed to any game object for more flexible but weirder controls
-        if (plane.Raycast(ray, out distance))
+        
+        if (_plane.Raycast(ray, out distance))
         {
             worldPosition = ray.GetPoint(distance);
 
             Vector3 direction = worldPosition - transform.position;
 
-            // make sure the direction is not exactly zero to avoid errors
             if (direction != Vector3.zero)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(direction);
-
-                // apply 90-degree offset on the X-axis due to model orientation
                 targetRotation *= Quaternion.Euler(90f, 0f, 0f);
-
-                // for smooth rotation towards mouse
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
             }
-
         }
     }
 }
